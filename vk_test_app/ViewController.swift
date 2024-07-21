b52c2acf34d905f87c6f3a93455a2ee314125ee4
Александр Соколов <133583532+fightersubmarine@users.noreply.github.com>
2024-07-21 13:06:00 +0300

    Initial Commit

---
+++ vk_test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk_test_app
+//
+//  Created by Алина on 21.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
